9076589832a836d42eced73937a9977c5da0f367
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-03-20 20:39:54 +0530

    Update page.js

--- app/tenants/page.js
+++ app/tenants/page.js
@@ -1,19 +1,27 @@
-
 import Navbar from "@/components/Navbar";
 import TenantForm from "@/components/TenantForm";
 
-export default async function Page(){
-  const tenants=await fetch("http://localhost:3000/api/tenants").then(r=>r.json());
+export const dynamic = "force-dynamic";
 
-  return(
+export default async function Page() {
+  const tenants = await fetch("/api/tenants", { cache: "no-store" }).then(res =>
+    res.json()
+  );
+
+  return (
     <div>
-      <Navbar/>
+      <Navbar />
       <div className="p-4">
-        <TenantForm/>
-        <ul className="mt-4">
-          {tenants.map(t=><li key={t._id}>{t.name} - {t.roomNumber}</li>)}
+        <TenantForm />
+
+        <ul className="mt-4 space-y-2">
+          {tenants.map((t) => (
+            <li key={t._id} className="bg-white p-2 rounded shadow">
+              {t.name} - {t.roomNumber}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
-  )
+  );
 }
