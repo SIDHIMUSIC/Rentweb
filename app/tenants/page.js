04f24ce31738baf8f245beb9d18cec045fef47be
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-03-20 20:59:20 +0530

    Update page.js

--- app/tenants/page.js
+++ app/tenants/page.js
@@ -1,12 +1,13 @@
 import Navbar from "@/components/Navbar";
 import TenantForm from "@/components/TenantForm";
+import { connectDB } from "@/lib/mongodb";
+import Tenant from "@/models/Tenant";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  const tenants = await fetch("/api/tenants", { cache: "no-store" }).then(res =>
-    res.json()
-  );
+  await connectDB();
+  const tenants = await Tenant.find();
 
   return (
     <div>
@@ -14,11 +15,9 @@ export default async function Page() {
       <div className="p-4">
         <TenantForm />
 
-        <ul className="mt-4 space-y-2">
-          {tenants.map((t) => (
-            <li key={t._id} className="bg-white p-2 rounded shadow">
-              {t.name} - {t.roomNumber}
-            </li>
+        <ul className="mt-4">
+          {tenants.map(t => (
+            <li key={t._id}>{t.name} - {t.roomNumber}</li>
           ))}
         </ul>
       </div>
