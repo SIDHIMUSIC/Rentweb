ce5d61fa849d1e23a22f9679880326b8d75dab62
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-03-21 08:56:34 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
-+export default function Navbar() {
+export default function Navbar() {
   return (
     <div className="bg-blue-600 text-white px-8 py-4 flex justify-between items-center shadow-md">
-
+      
       <h1 className="font-bold text-lg">🏠 Rent App</h1>
 
       <div className="flex gap-8 text-sm font-medium">
