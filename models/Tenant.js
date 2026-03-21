bc4d1d0ff89368ae726b4e684e4781ea44e41e80
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-03-21 11:52:41 +0530

    Update Tenant.js

--- models/Tenant.js
+++ models/Tenant.js
@@ -1,9 +1,12 @@
-
 import mongoose from "mongoose";
-const schema = new mongoose.Schema({
-  name:String,
-  phone:String,
-  roomNumber:String,
-  rentAmount:{type:Number,default:3000}
+
+const TenantSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  roomNumber: String,
+  rentAmount: Number,
+  startDate: Date, // 🔥 NEW
 });
-export default mongoose.models.Tenant || mongoose.model("Tenant",schema);
+
+export default mongoose.models.Tenant ||
+  mongoose.model("Tenant", TenantSchema);
