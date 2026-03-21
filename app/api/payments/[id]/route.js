352e8bc908c9ad7659fae7f0ee0afc9dc8a43ee5
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-03-21 12:57:22 +0530

    Update route.js

--- app/api/payments/[id]/route.js
+++ app/api/payments/[id]/route.js
@@ -1,10 +1,15 @@
 import { connectDB } from "../../../../lib/mongodb";
 import Payment from "../../../../models/Payment";
 
+// ✅ MARK AS FULL PAID
 export async function PUT(req, { params }) {
   await connectDB();
 
   const payment = await Payment.findById(params.id);
+
+  if (!payment) {
+    return Response.json({ success: false });
+  }
 
   payment.paidAmount = payment.totalRent;
   payment.remainingAmount = 0;
