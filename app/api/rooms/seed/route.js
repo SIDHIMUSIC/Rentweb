1d3fbbb12c3c633a05f4c011defc490293d14c8d
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-03-21 10:31:07 +0530

    Create route.js

---
+++ app/api/rooms/seed/route.js
@@ -0,0 +1,25 @@
+import { connectDB } from "../../../../lib/mongodb";
+import Room from "../../../../models/Room";
+
+export async function GET() {
+  await connectDB();
+
+  // delete old
+  await Room.deleteMany();
+
+  const rooms = [];
+
+  for (let f = 1; f <= 3; f++) {
+    for (let r = 1; r <= 10; r++) {
+      rooms.push({
+        roomNumber: `F${f}-R${r}`,
+        status: "vacant",
+        rent: 3000,
+      });
+    }
+  }
+
+  await Room.insertMany(rooms);
+
+  return Response.json({ message: "Rooms created ✅" });
+}
