1c17bc9f688176c224b7ab22a382386de1ec2516
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-03-21 11:26:25 +0530

    Update Payment.js

--- models/Payment.js
+++ models/Payment.js
@@ -1,12 +1,13 @@
-
 import mongoose from "mongoose";
-const schema = new mongoose.Schema({
-  tenant:{type:mongoose.Schema.Types.ObjectId,ref:"Tenant"},
-  month:String,
-  totalRent:Number,
-  paidAmount:Number,
-  remainingAmount:Number,
-  status:String,
-  paymentMode:String
+
+const PaymentSchema = new mongoose.Schema({
+  tenant: { type: mongoose.Schema.Types.ObjectId, ref: "Tenant" },
+  month: String,
+  totalRent: { type: Number, default: 3000 },
+  paidAmount: Number,
+  remainingAmount: Number,
+  status: String,
 });
-export default mongoose.models.Payment || mongoose.model("Payment",schema);
+
+export default mongoose.models.Payment ||
+  mongoose.model("Payment", PaymentSchema);
