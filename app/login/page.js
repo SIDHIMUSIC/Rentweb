0556a02fbac80db0e4b365f116870156a3e02bdb
sɪᴅʜɪ ᴍᴜsɪᴄ <harryheruki@gmail.com>
2026-03-23 21:27:03 +0530

    Update and rename page to page.js

---
+++ app/login/page.js
@@ -0,0 +1,52 @@
+"use client";
+import { useState } from "react";
+
+export default function Login() {
+  const [form, setForm] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleLogin = async () => {
+    const res = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    });
+
+    const data = await res.json();
+
+    if (data.success) {
+      localStorage.setItem("token", data.token);
+      alert("Login success ✅");
+      window.location.href = "/";
+    } else {
+      alert("Wrong login ❌");
+    }
+  };
+
+  return (
+    <div className="p-10">
+      <h1>Login</h1>
+
+      <input
+        placeholder="Username"
+        onChange={(e) =>
+          setForm({ ...form, username: e.target.value })
+        }
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) =>
+          setForm({ ...form, password: e.target.value })
+        }
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
